16b984d7da7454ad81e01dc894284443a2e67865
Shane Sarte <shnsarte@gmail.com>
2025-10-22 20:33:14 +0800

    new.html

---
+++ Lab 11/script.js
@@ -0,0 +1,13 @@
+// A simple console.log output
+console.log("Hello! This is my JavaScript output.");
+
+// A variable declaration using let
+let studentName = "Shane";
+console.log("Student Name: " + studentName);
+
+// A math computation using two variables
+let num1 = 12;
+let num2 = 8;
+let total = num1 + num2;
+
+console.log("The total of " + num1 + " and " + num2 + " is: " + total);
